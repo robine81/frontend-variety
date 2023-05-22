488314f4547726a715b29dd1f6dc2e3cf225469e
“Ilke <ilkedidem@gmail.com>
2023-05-22 16:36:27 +0200

    done listallevents feature, styling events page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import { Route, Routes } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-import ProfilePage from './pages/ProfilePage'
-import PrivateRoute from './components/PrivateRoute'
-import HomePage from './pages/HomePage'
-import  { Breakpoint, BreakpointProvider } from 'react-socks';
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProfilePage from "./pages/ProfilePage";
+import PrivateRoute from "./components/PrivateRoute";
+import HomePage from "./pages/HomePage";
+import { Breakpoint, BreakpointProvider } from "react-socks";
+import EventsPage from "./pages/EventsPage";
 
 function App() {
   return (
     <BreakpointProvider>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/signup' element={<SignupPage />} />
-      <Route path='/login' element={<LoginPage />} />
-      { <Route
-        path='/profile'
-        element={
-          <PrivateRoute>
-            <ProfilePage />
-          </PrivateRoute>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/events" element={<EventsPage />} />
+        {
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <ProfilePage />
+              </PrivateRoute>
+            }
+          />
         }
-      /> }
-    </Routes>
+      </Routes>
     </BreakpointProvider>
-  )
+  );
 }
 
-export default App
+export default App;
