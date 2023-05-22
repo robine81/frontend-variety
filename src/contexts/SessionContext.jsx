7002dd8111cb5f7e3b29339edafb38ee2bfd53a2
“Ilke <ilkedidem@gmail.com>
2023-05-22 23:22:18 +0200

    stores user in session context

--- src/contexts/SessionContext.jsx
+++ src/contexts/SessionContext.jsx
@@ -1,63 +1,75 @@
-import { useLocalStorage } from '@mantine/hooks'
-import { createContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocalStorage } from "@mantine/hooks";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const SessionContext = createContext()
+export const SessionContext = createContext();
 
 const SessionContextProvider = ({ children }) => {
-  const [token, setToken] = useLocalStorage({ key: 'authToken' })
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
+  const [token, setToken] = useLocalStorage({ key: "authToken" });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const verifyToken = async currentToken => {
-    const response = await fetch('http://localhost:5005/auth/verify', {
+  const verifyToken = async (currentToken) => {
+    const response = await fetch("http://localhost:5005/auth/verify", {
       headers: {
         Authorization: `Bearer ${currentToken}`,
       },
-    })
-    console.log("Respones: ", response.ok)
+    });
+    console.log("Respones: ", response.ok);
     if (response.ok) {
-      const parsed = await response.json()
-      setToken(currentToken)
-      setIsLoggedIn(true)
-      console.log("Parsed: ",parsed)
+      const parsed = await response.json();
+      setToken(currentToken);
+      setIsLoggedIn(true);
+      console.log("Parsed: ", parsed);
     }
-     setIsLoading(false)
-  }
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    const localToken = localStorage.getItem('authToken')
+    const localToken = localStorage.getItem("authToken");
     if (localToken) {
-      verifyToken(localToken)
+      verifyToken(localToken);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (token) {
-      localStorage.setItem('authToken', token)
+      localStorage.setItem("authToken", token);
       // setIsLoggedIn(true)
-      setIsLoading(false)
+      setIsLoading(false);
     } else {
-      localStorage.removeItem('authToken')
+      localStorage.removeItem("authToken");
       // setIsLoading(false)
     }
-  }, [token])
+  }, [token]);
 
   const logout = () => {
-    setToken()
-    localStorage.removeItem('authToken')
-    setIsLoggedIn(false)
-    console.log("Logged in: ", isLoggedIn)
-    navigate('/login')
-  }
+    setToken();
+    localStorage.removeItem("authToken");
+    setIsLoggedIn(false);
+    console.log("Logged in: ", isLoggedIn);
+    navigate("/login");
+  };
 
   return (
-    <SessionContext.Provider value={{ token, setToken, isLoggedIn, setIsLoggedIn, isLoading, logout }}>
+    <SessionContext.Provider
+      value={{
+        token,
+        setToken,
+        isLoggedIn,
+        setIsLoggedIn,
+        isLoading,
+        logout,
+        user,
+        setUser,
+      }}
+    >
       {children}
     </SessionContext.Provider>
-  )
-}
+  );
+};
 
-export default SessionContextProvider
+export default SessionContextProvider;
