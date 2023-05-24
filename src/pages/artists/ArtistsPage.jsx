d7de9b5094b7dc6d3665eec6007141bc2439c888
Federico <federicofarina@hotmail.com>
2023-05-24 17:00:26 +0200

    Styling in Artists

--- src/pages/artists/ArtistsPage.jsx
+++ src/pages/artists/ArtistsPage.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import "./artists.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const ArtistsPage = () => {
     const [ artists, setArtists ]= useState([])
@@ -21,19 +26,26 @@ const ArtistsPage = () => {
 
     return artists ? (
         <>
-        <h1>Variety Artists</h1>
-        {artists.map(artist => (
-            <Link key={artist._id} to ={`/artists/${artist._id}`}>
-                <img width="150px" src={artist.artistPicUrl} alt='artist image'/>
-                <h2>{artist.artistName}</h2>
-                <h4>{artist.soundCloudUrl}</h4>
-            </Link>
-        ))}
-            <div>
-              <button>
-                 <Link to = "/artists/add">Add a new Artist </Link>
-              </button>
-            </div>    
+          <h1 className="page-title">Variety Artists</h1>
+          <div className="artist-list">
+            {artists.map(artist => (
+              <Link key={artist._id} to={`/artists/${artist._id}`} className="artist-link">
+                <div className="artist-card">
+                  <img className="artist-image" src={artist.artistPicUrl} alt='artist image' />
+                  <h2 className="artist-name">{artist.artistName}</h2>
+                  <h2 className="artist-url">
+                  <a href={artist.soundCloudUrl} target="_blank" rel="noopener noreferrer">Soundcloud</a></h2>
+                  <h2 className="artist-url">
+                    <a href={artist.instagramUrl} target="_blank" rel="noopener noreferrer">Instagram</a></h2>
+                </div>
+              </Link>
+            ))}
+          </div>
+          <div>
+            <button className="add-artist-btn">
+              <Link to="/artists/add" variant="outlined">Add a new Artist</Link>
+            </button>
+          </div>
         </>
       ) : (
         <h1>Loading...</h1>
