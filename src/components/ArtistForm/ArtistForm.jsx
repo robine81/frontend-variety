4e59e424002b7aa9d4e0a8ae4ee38373d755aa73
“Ilke <ilkedidem@gmail.com>
2023-05-25 17:37:27 +0200

    artist form

---
+++ src/components/ArtistForm/ArtistForm.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { SessionContext } from "../../contexts/SessionContext";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+export default function ArtistForm(props) {
+  const { handleSubmit, isUpdate } = props;
+  const { token } = useContext(SessionContext);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    artistName: "",
+    firstName: "",
+    lastName: "",
+    artistPicUrl: "",
+    soundCloudUrl: "",
+    beatPortUrl: "",
+    instagramUrl: "",
+    facebookUrl: "",
+    webPage: "",
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchArtist = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BASE_API_URL}/artists/${id}`
+      );
+      const artist = await response.json();
+      delete artist.id;
+      //console.log(artist)
+      //console.log(inputs)
+      setIsLoading(false);
+      setInputs(artist);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (isUpdate) {
+      fetchArtist();
+    }
+  }, [isUpdate, id]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputs((prevInputs) => ({
+      ...prevInputs,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form
+      style={{ display: "grid", gridTemplate: "repeat(5, 1fr) / auto" }}
+      onSubmit={handleSubmit}
+    >
+      <TextField
+        label="Artist Name"
+        variant="outlined"
+        value={inputs.artistName}
+        name="artistName"
+        onChange={handleChange}
+      />
+      <TextField
+        label="First Name"
+        variant="outlined"
+        value={inputs.firstName}
+        name="firstName"
+        onChange={handleChange}
+      />
+      <TextField
+        label="Last Name"
+        variant="outlined"
+        value={inputs.lastName}
+        name="lastName"
+        onChange={handleChange}
+      />
+      <TextField
+        label="Artist Picture"
+        variant="outlined"
+        value={inputs.artistPicUrl}
+        name="artistPicUrl"
+        onChange={handleChange}
+      />
+      <TextField
+        label="Soundcloud"
+        variant="outlined"
+        value={inputs.soundCloudUrl}
+        name="soundCloudUrl"
+        onChange={handleChange}
+      />
+      <TextField
+        label="Beatport"
+        variant="outlined"
+        value={inputs.beatPortUrl}
+        name="beatPortUrl"
+        onChange={handleChange}
+      />
+      <TextField
+        label="Instagram"
+        variant="outlined"
+        value={inputs.instagramUrl}
+        name="instagramUrl"
+        onChange={handleChange}
+      />
+      <TextField
+        label="Facebook"
+        variant="outlined"
+        value={inputs.facebookUrl}
+        name="facebookUrl"
+        onChange={handleChange}
+      />
+      <TextField
+        label="Web Page"
+        variant="outlined"
+        value={inputs.webPage}
+        name="webPage"
+        onChange={handleChange}
+      />
+      <Button variant="contained" type="submit">
+        {isUpdate ? "UPDATE" : "ADD NEW"}
+      </Button>
+      <button type="submit">Save Changes</button>
+    </form>
+  );
+}
--- src/pages/artists/AddArtistPage.jsx
+++ src/pages/artists/AddArtistPage.jsx
@@ -1,117 +1,53 @@
-import { useState, useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { SessionContext } from "../../contexts/SessionContext";
-
+import ArtistForm from "../../components/ArtistForm/ArtistForm";
 
 const AddArtistPage = () => {
-    const { token } = useContext(SessionContext);
-    const navigate = useNavigate()
-    const [artistName, setArtistName] = useState('')
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [artistPicUrl, setArtistPicUrl] = useState('')
-    const [soundCloudUrl, setSoundCloudUrl] = useState('')
-    const [beatPortUrl, setBeatPortUrl] = useState('')
-    const [instagramUrl, setInstagramUrl] = useState('')
-    const [facebookUrl, setFacebookUrl] = useState('')
-    const [webPage, setWebPage] = useState('')
+  const { token } = useContext(SessionContext);
+  const navigate = useNavigate();
+  const [artistName, setArtistName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [artistPicUrl, setArtistPicUrl] = useState("");
+  const [soundCloudUrl, setSoundCloudUrl] = useState("");
+  const [beatPortUrl, setBeatPortUrl] = useState("");
+  const [instagramUrl, setInstagramUrl] = useState("");
+  const [facebookUrl, setFacebookUrl] = useState("");
+  const [webPage, setWebPage] = useState("");
 
-    const handleSubmit = async e => {
-        e.preventDefault()
-        const newArtist = {
-            artistName,
-            firstName,
-            lastName,
-            artistPicUrl,
-            soundCloudUrl,
-            beatPortUrl,
-            instagramUrl,
-            facebookUrl,
-            webPage,
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newArtist = {
+      artistName,
+      firstName,
+      lastName,
+      artistPicUrl,
+      soundCloudUrl,
+      beatPortUrl,
+      instagramUrl,
+      facebookUrl,
+      webPage,
+    };
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BASE_API_URL}/artists/add`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(newArtist),
         }
-        try {
-            const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/artists/add`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                      Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify(newArtist),
-            });
-
-                console.log('Artist created:', response.json())
-                navigate(`/artists`)
-            }
-         catch (error) {
-            console.log(error)
-        }
-    }
-    return (
-        <div>
-            <h1>Add a new Artist</h1>
-
-            <form
-            style={{ display: 'grid', gridTemplate: 'repeat(5, 1fr) / auto' }}
-            onSubmit={handleSubmit}>
-        <label>Artist Name</label>
-        <input
-          type="text"
-          value={artistName}
-          onChange={(e) => setArtistName(e.target.value)}
-        />
-        <label>First Name</label>
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <label>Last Name</label>
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <label>Profile Image</label>
-        <input
-          type="text"
-          value={artistPicUrl}
-          onChange={(e) => setArtistPicUrl(e.target.value)}
-        />
-        <label>Soundcloud</label>
-        <input
-          type="text"
-          value={soundCloudUrl}
-          onChange={(e) => setSoundCloudUrl(e.target.value)}
-        />
-        <label>Beatport</label>
-        <input
-          type="text"
-          value={beatPortUrl}
-          onChange={(e) => setBeatPortUrl(e.target.value)}
-        />
-        <label>Instagram</label>
-        <input
-          type="text"
-          value={instagramUrl}
-          onChange={(e) => setInstagramUrl(e.target.value)}
-        />
-        <label>Facebook</label>
-        <input
-          type="text"
-          value={facebookUrl}
-          onChange={(e) => setFacebookUrl(e.target.value)}
-        />
-        <label>Web Page</label>
-        <input
-          type="text"
-          value={webPage}
-          onChange={(e) => setWebPage(e.target.value)}
-        />
-        <button type="submit">Add Artist</button>
-            </form>
-
-        </div>
       );
 
+      console.log("Artist created:", response.json());
+      navigate(`/artists`);
+    } catch (error) {
+      console.log(error);
     }
+  };
+  return <ArtistForm />;
+};
 export default AddArtistPage;
