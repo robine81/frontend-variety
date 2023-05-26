752594346040cb48052e2321ad52b185ab2ed6d2
“Ilke <ilkedidem@gmail.com>
2023-05-26 09:07:49 +0200

    done login signup profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,12 @@
 import { useContext, useState } from "react";
 import { SessionContext } from "../contexts/SessionContext";
 import { useNavigate, useParams } from "react-router-dom";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 const ProfilePage = () => {
   const { logout } = useContext(SessionContext);
-  const { isLoggedIn } = useContext(SessionContext);
   const { user } = useContext(SessionContext);
-  const [isLoading, setIsLoading] = useState(true)
 
   const navigate = useNavigate();
 
@@ -17,58 +17,70 @@ const ProfilePage = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    try 
-    {    
+    try {
       const response = await fetch(
-      `${import.meta.env.VITE_BASE_API_URL}/profile/edit`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, firstName, lastName }),
+        `${import.meta.env.VITE_BASE_API_URL}/profile/edit`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, firstName, lastName }),
+        }
+      );
+      if (response.status === 201) {
+        navigate("/profile");
       }
-    );
-    if (response.status === 201) {
-      navigate("/profile");
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error){
-    console.log(error)
-  }
   };
 
   return (
     <>
       <h1>My Profile</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name
-          <input
-            type="text"
-            value={firstName}
-            onChange={(event) => setFirstName(event.target.value)}
-          />
-        </label>
-        <label>
-          Last Name
-          <input
-            type="text"
-            value={lastName}
-            onChange={(event) => setLastName(event.target.value)}
-          />
-        </label>
-        <label>
-          Email
-          <input
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </label>
-        <button type="submit">Edit</button>
-        <button type="button" onClick={logout}>
-          Logout
-        </button>
+      <form className="login-signup-form" onSubmit={handleSubmit}>
+        <TextField
+          label="First Name"
+          variant="outlined"
+          type="text"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+        <TextField
+          label="Last Name"
+          variant="outlined"
+          type="text"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+        <TextField
+          label="Email"
+          variant="outlined"
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <div className="profile-button-group">
+          <Button variant="contained" type="submit">
+            Edit
+          </Button>
+          <Button
+            variant="contained"
+            type="button"
+            color="error"
+            onClick={logout}
+          >
+            Logout
+          </Button>
+        </div>
       </form>
     </>
   );
